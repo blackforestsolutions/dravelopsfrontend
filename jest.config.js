766daafb632775f3ccbab22c6fa233e3914eb05c
hennihaus <jan-hendrik.hausner@outlook.com>
2021-06-04 15:33:34 +0200

    initial commit #patch

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/shell',
+    '<rootDir>/apps/efa',
+    '<rootDir>/apps/booking',
+    '<rootDir>/libs/shared-styles',
+    '<rootDir>/libs/data-access',
+  ],
+};
