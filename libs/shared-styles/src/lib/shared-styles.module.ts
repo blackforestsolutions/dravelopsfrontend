34bcb6802090c53dd22dce3fc4d304f135348632
hennihaus <jan-hendrik.hausner@outlook.com>
2021-06-14 13:58:24 +0200

    added shared modules to commit #minor

--- libs/shared-styles/src/lib/shared-styles.module.ts
+++ libs/shared-styles/src/lib/shared-styles.module.ts
@@ -1,7 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
+import {ThemeEmitterComponent} from './components/theme-emitter/theme-emitter.component';
+import {LoadingComponent} from './components/loading/loading.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+
+const materialModules = [
+  MatButtonModule,
+  MatProgressSpinnerModule
+];
+
+const components = [
+  ThemeEmitterComponent,
+  LoadingComponent
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ...materialModules
+  ],
+  declarations: [
+    ...components
+  ],
+  exports: [
+    ...components
+  ]
 })
-export class SharedStylesModule {}
+export class SharedStylesModule {
+}
