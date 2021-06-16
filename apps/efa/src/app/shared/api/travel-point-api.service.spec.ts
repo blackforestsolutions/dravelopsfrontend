dc3873a21e71fcbf83fb787130c86e463ec0bed1
hennihaus <jan-hendrik.hausner@outlook.com>
2021-06-16 09:57:52 +0200

    added tests for TravelPointApiService #patch

--- apps/efa/src/app/shared/api/travel-point-api.service.spec.ts
+++ apps/efa/src/app/shared/api/travel-point-api.service.spec.ts
@@ -1,25 +1,69 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TravelPointApiService } from './travel-point-api.service';
-import { GetAddressesGQL } from '@dravelopsfrontend/generated-content';
-import { getFurtwangenUniversityTravelPoint } from '../objectmothers/travel-point-object-mother';
+import {
+  AutocompleteAddressFragment,
+  GetAddressesGQL,
+  GetNearestAddressesGQL,
+  NearestAddressFragment
+} from '@dravelopsfrontend/generated-content';
+import {
+  getFurtwangenFriedrichStreetOneTravelPoint,
+  getFurtwangenFriedrichStreetThreeTravelPoint,
+  getFurtwangenFriedrichStreetTwoTravelPoint,
+  getFurtwangenKindergardenTravelPoint,
+  getFurtwangenSupermarketTravelPoint,
+  getFurtwangenUniversityTravelPoint
+} from '../objectmothers/travel-point-object-mother';
 import { of } from 'rxjs';
 import { expect } from '@jest/globals';
+import { RADIUS_IN_KILOMETERS } from '../../../environments/config-tokens';
+
+const TEST_RADIUS_IN_KILOMETERS = 5;
 
 describe('TravelPointApiService', () => {
   let getAddressesGQLSpy: jasmine.Spy;
+  let getNearestAddressesGQLSpy: jasmine.Spy;
   let classUnderTest: TravelPointApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: RADIUS_IN_KILOMETERS,
+          useValue: TEST_RADIUS_IN_KILOMETERS
+        }
+      ]
+    });
     const getAddressesGQL: GetAddressesGQL = TestBed.inject(GetAddressesGQL);
+    classUnderTest = TestBed.inject(TravelPointApiService);
+    const getNearestAddressesGQl: GetNearestAddressesGQL = TestBed.inject(GetNearestAddressesGQL);
     classUnderTest = TestBed.inject(TravelPointApiService);
 
     getAddressesGQLSpy = spyOn(getAddressesGQL, 'watch').and.returnValue(
       {
         valueChanges: of({
           data: {
-            getAutocompleteAddressesBy: [getFurtwangenUniversityTravelPoint(), getFurtwangenUniversityTravelPoint()]
+            getAutocompleteAddressesBy: [
+              getFurtwangenUniversityTravelPoint(),
+              getFurtwangenUniversityTravelPoint()
+            ]
+          }
+        })
+      }
+    );
+
+    getNearestAddressesGQLSpy = spyOn(getNearestAddressesGQl, 'watch').and.returnValue(
+      {
+        valueChanges: of({
+          data: {
+            getNearestAddressesBy: [
+              getFurtwangenSupermarketTravelPoint(),
+              getFurtwangenKindergardenTravelPoint(),
+              getFurtwangenFriedrichStreetOneTravelPoint(),
+              getFurtwangenFriedrichStreetTwoTravelPoint(),
+              getFurtwangenFriedrichStreetThreeTravelPoint()
+            ]
           }
         })
       }
@@ -30,10 +74,10 @@ describe('TravelPointApiService', () => {
     expect(classUnderTest).toBeTruthy();
   });
 
-  it('should GET a list of TravelPoints', (done) => {
+  it('should GET a list of TravelPoints when getAddressesBy text is called', (done) => {
     const testText = 'Am Großhausberg 8';
 
-    classUnderTest.getAddressesBy(testText).subscribe((result) => {
+    classUnderTest.getAddressesBy(testText).subscribe((result: AutocompleteAddressFragment[]) => {
       expect(result.length).toBe(2);
       expect(result[0]).toEqual(getFurtwangenUniversityTravelPoint());
       expect(result[1]).toEqual(getFurtwangenUniversityTravelPoint());
@@ -41,12 +85,45 @@ describe('TravelPointApiService', () => {
     });
   });
 
-  it('should be called "getAddressesGQL" correctly and with right params', () => {
+  it('should be called "getAddressesGQL" correctly and with right params', (done) => {
     const testText = 'Am Großhausberg 8';
 
     classUnderTest.getAddressesBy(testText).subscribe(() => {
       expect(getAddressesGQLSpy).toHaveBeenCalledTimes(1);
-      expect(getAddressesGQLSpy).toHaveBeenCalledWith(testText);
+      expect(getAddressesGQLSpy).toHaveBeenCalledWith({
+        text: testText
+      });
+      done();
+    });
+  });
+
+  it('should GET a list of travelPoints when getNearestAddressesBy longitude and latitude is called', (done) => {
+    const testLongitude = 10.0;
+    const testLatitude = 10.0;
+
+    classUnderTest.getNearestAddressesBy(testLongitude, testLatitude).subscribe((result: NearestAddressFragment[]) => {
+      expect(result.length).toBe(5);
+      expect(result[0]).toEqual(getFurtwangenSupermarketTravelPoint());
+      expect(result[1]).toEqual(getFurtwangenKindergardenTravelPoint());
+      expect(result[2]).toEqual(getFurtwangenFriedrichStreetOneTravelPoint());
+      expect(result[3]).toEqual(getFurtwangenFriedrichStreetTwoTravelPoint());
+      expect(result[4]).toEqual(getFurtwangenFriedrichStreetThreeTravelPoint());
+      done();
+    });
+  });
+
+  it('should be called "getNearestAddressesGQL" correctly and with right params', (done) => {
+    const testLongitude = 10.0;
+    const testLatitude = 10.0;
+
+    classUnderTest.getNearestAddressesBy(testLongitude, testLatitude).subscribe(() => {
+      expect(getNearestAddressesGQLSpy).toHaveBeenCalledTimes(1);
+      expect(getNearestAddressesGQLSpy).toHaveBeenCalledWith({
+        longitude: testLongitude,
+        latitude: testLatitude,
+        radiusInKilometers: TEST_RADIUS_IN_KILOMETERS
+      });
+      done();
     });
   });
 });
--- apps/efa/src/app/shared/api/travel-point-api.service.ts
+++ apps/efa/src/app/shared/api/travel-point-api.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
-import { AutocompleteTravelPointFragment, GetAddressesGQL, GetAddressesQuery } from '@dravelopsfrontend/generated-content';
-import { map, retry } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { ApolloQueryResult } from '@apollo/client';
+import {Inject, Injectable} from '@angular/core';
+import {
+  AutocompleteAddressFragment,
+  GetAddressesGQL,
+  GetAddressesQuery,
+  GetNearestAddressesGQL,
+  GetNearestAddressesQuery,
+  NearestAddressFragment
+} from '@dravelopsfrontend/generated-content';
+import {map, retry} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {ApolloQueryResult} from '@apollo/client';
+import {RADIUS_IN_KILOMETERS} from "../../../environments/config-tokens";
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +18,33 @@ import { ApolloQueryResult } from '@apollo/client';
 export class TravelPointApiService {
 
   constructor(
-    private readonly getAddressesGQL: GetAddressesGQL
+    @Inject(RADIUS_IN_KILOMETERS) private readonly radiusInKilometers: number,
+    private readonly getAddressesGQL: GetAddressesGQL,
+    private readonly getNearestAddressesGQL: GetNearestAddressesGQL
   ) {
   }
 
-  getAddressesBy(text: string): Observable<AutocompleteTravelPointFragment[]> {
+  getAddressesBy(text: string): Observable<AutocompleteAddressFragment[]> {
     return this.getAddressesGQL
-      .watch({ text })
+      .watch({text})
       .valueChanges
       .pipe(
         retry(3),
         map((result: ApolloQueryResult<GetAddressesQuery>) => result.data.getAutocompleteAddressesBy)
       );
+  }
+
+  getNearestAddressesBy(longitude: number, latitude: number): Observable<NearestAddressFragment[]> {
+    return this.getNearestAddressesGQL
+      .watch({
+        longitude,
+        latitude,
+        radiusInKilometers: this.radiusInKilometers,
+      })
+      .valueChanges
+      .pipe(
+        retry(3),
+        map((result: ApolloQueryResult<GetNearestAddressesQuery>) => result.data.getNearestAddressesBy)
+      )
   }
 }
