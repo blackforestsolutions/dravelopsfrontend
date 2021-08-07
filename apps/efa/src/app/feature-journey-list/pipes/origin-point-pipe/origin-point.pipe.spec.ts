da063ffc56787cb607a0b9266598d4c45fb9e933
hennihaus <jan-hendrik.hausner@outlook.com>
2021-08-07 17:41:32 +0200

    added tests for origin point pipe #minor

---
+++ apps/efa/src/app/feature-journey-list/pipes/origin-point-pipe/origin-point.pipe.spec.ts
@@ -0,0 +1,69 @@
+import { OriginPointPipe } from './origin-point.pipe';
+import { CompassDirectionPipe } from '../compass-direction-pipe/compass-direction.pipe';
+import { WalkStep } from '@dravelopsfrontend/generated-content';
+
+describe('OriginPointPipe', () => {
+
+  const compassDirectionPipe = new CompassDirectionPipe();
+
+  const pipeUnderTest: OriginPointPipe = new OriginPointPipe(compassDirectionPipe);
+
+  beforeEach(() => spyOn(compassDirectionPipe, 'transform').and.returnValue('Norden'));
+
+  it('create an instance', () => {
+    expect(pipeUnderTest).toBeTruthy();
+  });
+
+  it('should return correct message walkStep street name is generated and nextWalkStep is null', () => {
+    const testWalkStep: WalkStep = {
+      isStreetNameGenerated: true
+    };
+    const testNextWalkStep: WalkStep = null;
+
+    const result: string = pipeUnderTest.transform(testWalkStep, testNextWalkStep);
+
+    expect(result).toBe('Start nach Norden');
+  });
+
+  it('should return correct message walkStep street name is generated and nextWalkStep street name is generated', () => {
+    const testWalkStep: WalkStep = {
+      isStreetNameGenerated: true
+    };
+    const testNextWalkStep: WalkStep = {
+      isStreetNameGenerated: true
+    };
+
+    const result: string = pipeUnderTest.transform(testWalkStep, testNextWalkStep);
+
+    expect(result).toBe('Start nach Norden');
+  });
+
+  it('should return correct message walkStep street name is generated and nextWalkStep street name is not generated', () => {
+    const testWalkStep: WalkStep = {
+      isStreetNameGenerated: true
+    };
+    const testNextWalkStep: WalkStep = {
+      isStreetNameGenerated: false,
+      streetName: 'Tanneck 1B'
+    };
+
+    const result: string = pipeUnderTest.transform(testWalkStep, testNextWalkStep);
+
+    expect(result).toBe('Start nach Norden Richtung Tanneck 1B');
+  });
+
+  it('should return correct message walkStep street name is not generated and nextWalkStep street name is not generated', () => {
+    const testWalkStep: WalkStep = {
+      isStreetNameGenerated: false,
+      streetName: 'Eiderstraße'
+    };
+    const testNextWalkStep: WalkStep = {
+      isStreetNameGenerated: false,
+      streetName: 'Tanneck 1B'
+    };
+
+    const result: string = pipeUnderTest.transform(testWalkStep, testNextWalkStep);
+
+    expect(result).toBe('Start auf Eiderstraße nach Norden Richtung Tanneck 1B');
+  });
+});
