ee8853ecbdc45d23457ca16ebe8de61144defc22
hennihaus <jan-hendrik.hausner@outlook.com>
2022-01-06 13:51:16 +0100

    I made journey search module responsive for all devices. I added an autofocus directive to focus angular material input when component is initialized #patch

---
+++ libs/shared/src/lib/directives/autofocus/autofocus.directive.spec.ts
@@ -0,0 +1,40 @@
+import { AutofocusDirective } from './autofocus.directive';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'dravelopssharedfrontend-test',
+  template: '<input type="text" matInput dravelopssharedfrontendAutofocus id="testInput">'
+})
+class TestComponent {
+}
+
+describe('AutofocusDirective', () => {
+  let testComponent: TestComponent;
+  let testFixture: ComponentFixture<TestComponent>;
+
+  let directiveUnderTest: AutofocusDirective;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective],
+      providers: [MatInput],
+      imports: [MatInputModule]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    testFixture = TestBed.createComponent(TestComponent);
+    testComponent = testFixture.componentInstance;
+    directiveUnderTest = testFixture.debugElement.query(By.directive(AutofocusDirective)).componentInstance;
+    testFixture.detectChanges();
+  });
+
+
+  it('should create an instance', () => {
+    expect(directiveUnderTest).toBeTruthy();
+  });
+});
