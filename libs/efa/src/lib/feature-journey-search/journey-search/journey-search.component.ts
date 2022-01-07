1e64500c6462d0a14d92d7bc618daf5a2bf890a6
hennihaus <jan-hendrik.hausner@outlook.com>
2022-01-07 14:33:22 +0100

    I made journey search module responsive for all devices. The journey-search.component does not show map-search in touch view anymore. #patch

--- libs/efa/src/lib/feature-journey-search/journey-search/journey-search.component.ts
+++ libs/efa/src/lib/feature-journey-search/journey-search/journey-search.component.ts
@@ -1,18 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ApiToken } from '../../domain/model/api-token';
 import { NearestTravelPointFragment } from '../../domain/model/generated';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { TOUCH_BREAKPOINT } from '@dravelopsfrontend/shared';
 
 @Component({
   selector: 'dravelopsefafrontend-journey-search',
   templateUrl: './journey-search.component.html',
   styleUrls: ['./journey-search.component.scss']
 })
-export class JourneySearchComponent {
+export class JourneySearchComponent implements OnInit {
   @Output() readonly submitApiTokenEvent = new EventEmitter<ApiToken>();
 
   selectedDeparture: NearestTravelPointFragment;
   selectedArrival: NearestTravelPointFragment;
   selectedTabIndex = 0;
+  isDesktopView$: Observable<boolean>;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.isDesktopView$ = this.breakpointObserver.observe(`(min-width: ${TOUCH_BREAKPOINT}px)`).pipe(
+      map((breakPointState: BreakpointState) => breakPointState.matches)
+    );
+  }
 
   handleDepartureSelectEvent(selectedDeparture: NearestTravelPointFragment): void {
     this.selectedDeparture = selectedDeparture;
